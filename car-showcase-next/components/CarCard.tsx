b28aefb02e75493c0b60fe99960c22b6787c4778
DESKTOP-VL7Q3DC\LahiruMadhawa <lahirumadhawa00@gmail.com>
2024-03-29 12:22:40 +1100

    car card component

---
+++ car-showcase-next/components/CarCard.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+const CarCard = () => {
+  return (
+    <div className="car-card group">
+      <div className="car-card__content">
+        <h2 className="car-card__content-title">
+          Make, Model
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default CarCard;
